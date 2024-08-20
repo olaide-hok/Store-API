5408f1274162ad9f2ea959ff55f7548e773c1f9d
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-08-20 15:39:49 +0100

    Setup filtering using pagination

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,10 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({}).sort('-name price');
+    const products = await Product.find({})
+        .sort('-name price')
+        .select('name price')
+        .limit(10);
     res.status(200).json({products, nbHits: products.length});
 };
 
@@ -33,6 +36,12 @@ const getAllProducts = async (req, res) => {
         const fieldsList = fields.split(',').join(' ');
         result = result.select(fieldsList);
     }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
 
     const products = await result;
     res.status(200).json({products, nbHits: products.length});
